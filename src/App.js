9a163be8f06ba71330483c7c9cc98ed5548e780d
Ume Aemun <81216378+umeaemun@users.noreply.github.com>
2022-03-20 00:56:01 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import {add, sub, mul, div} from "./Calc";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <ul>
+     <li>Sum is {add(1,3)}</li>
+     <li>Subtraction is {sub(4,2)}</li>
+     <li>Multiplication is {mul(4,2)}</li>
+     <li>Division is {div(4,2)}</li>
+   </ul>
+   </>
   );
 }
 
---
+++ src/Calc.js
@@ -0,0 +1,23 @@
+function add(a,b)
+{
+let ans=a+b;
+return ans;
+}
+
+function sub(a,b)
+{
+let ans=a-b;
+return ans;
+}
+function mul(a,b)
+{
+let ans=a*b;
+return ans;
+}
+function div(a,b)
+{
+let ans=a/b;
+return ans;
+}
+
+export {add, sub, mul, div} ;
